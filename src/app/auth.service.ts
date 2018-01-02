4e688e6524684e6be6464286c4d4362b84a370a5
Loïc MARIE <loic.marie54@gmail.com>
2018-01-02 11:56:48 +0100

    Fixed url in auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
     clientID: env.authClientId,
     domain: env.authDomain,
     responseType: 'token id_token',
-    audience: 'http://' + env.authDomain + '/userinfo',
+    audience: 'https://' + env.authDomain + '/userinfo',
     redirectUri: 'http://' + env.repertoDomain + ':' + env.repertoPort + '/',
     scope: 'openid profile'
   });
